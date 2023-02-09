97883834a2a160de3ec96408b4c82cbccb8eba3f
ramleeramlee <124111118+ramleeramlee@users.noreply.github.com>
2023-02-09 17:58:00 +0900

    day01

---
+++ javabasic/src/변수/MyHome.java
@@ -0,0 +1,21 @@
+package 변수;
+
+public class MyHome {
+
+	public static void main(String[] args) {
+		int myAge = 100;
+		//전화번호
+		String myTel = "01022226666";
+		//자릿수가 의미가 있는지 체크
+		
+		//주민번호
+		String ssn = "930206";
+				
+		//절대 값을 변하게 하지 말아줘!! 값을 설정(final)
+		//상수변수-- final, 변수명을 다 대문자로!!
+		final double PI = 3.14;		
+		
+
+	}
+
+}
---
+++ javabasic/src/제어문/문제풀이2.java
@@ -0,0 +1,17 @@
+package 제어문;
+
+import javax.swing.JOptionPane;
+
+public class 문제풀이2 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 30;
+		int num2 = 40;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+	}
+
+}
